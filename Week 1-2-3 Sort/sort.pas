f87e1f514f0825bf3b8d74ff9d52579b439193d2
German <terbiyarchitect@gmail.com>
2015-03-24 17:23:22 +0300

    Add several new queue functions, quicksort partly done

--- Week 1-2-3 Sort/sort.pas
+++ Week 1-2-3 Sort/sort.pas
@@ -1,12 +1,10 @@
 Program datesSort;
 uses crt;
-// Поддерживаемые форматы дат:
-// ЧЧ.ММ.ГГГГ
-// ЧЧ.ММ.ГГ
 
 // Константа для быстрой сортировки.
 const
   M = 1;
+  MinInt = - MaxInt - 1;
 
 type
   // Очередь для хранения значений дат.
@@ -38,9 +36,8 @@ var
 procedure push(var q, p: pQu; var d: dateYMD);
   var x, y: pQu;
 begin
-  // q - начало очереди.
-  y := q;
-  
+  // Функция работает с указателем, но не с началом очереди. Если передать в
+  // функцию начало, то в резултате указатель сместится на конец.
   new(x);
   x^.date := d;
   x^.next := nil;
@@ -49,50 +46,110 @@ begin
   if q = nil Then
     q := x
   else begin
-    While y^.next <> nil Do
-      y := y^.next;
+    if p = nil then p := q;
+    
+    While p^.next <> nil Do
+      p := p^.next;
 
-    y^.next := x;
-    x^.prev := y;
-    // Указатель на конец очереди.
+    p^.next := x;
+    x^.prev := p;
     p := x;
+  end
+end;
+
+procedure shift(var q: pQu; var d: dateYMD);
+  var x, y: pQu;
+begin
+  new(x);
+  x^.date := d;
+  x^.next := nil;
+  x^.prev := nil;
+
+  if q = nil Then q := x
+  else begin
+    While q^.prev <> nil do q := q^.prev;
+
+    q^.prev := x;
+    x^.next := q;
+    q := x
   end
 end;
 
 procedure exchange(var el1, el2: pQu);
   var buf: pQu;
 begin
-  new(buf);
-  // Сохранение связей первого элемента.
-  buf^.next := el1^.next;
-  buf^.prev := el1^.prev;
+  // Проверка, не один и тот же ли введён в функцию в качестве
+  // параметра элемент.
+  // Всё же процедура пока не достаточно объемлющая, так как её запуск
+  // осуществляется из предположения, что элементы
 
-  if (el1^.next = el2) OR (el2^.next = el1) Then Begin
-    if el2^.next = el1 then
-      exchange(el2, el1)
-    else begin
-      // Для соседних элементов.
-      el1^.next := el2^.next;
-      el2^.prev := el1^.prev;
-      el1^.prev := el2;
-      el2^.next := el1;
-      el2^.prev^.next := el2;
-      el1^.next^.prev := el1
+  // Данная функция реализует изменение самой структуры массива и, как
+  // следствие меняет порядковый номер элементов, на который смотрят
+  // указатели.
+  if (el1 <> el2) then begin
+    if (el1^.next = el2) OR (el2^.next = el1) Then Begin
+      if el2^.next = el1 then
+        exchange(el2, el1)
+      else begin
+        // Для соседних элементов.
+        el1^.next := el2^.next;
+        el2^.prev := el1^.prev;
+        el1^.prev := el2;
+        el2^.next := el1;
+        el2^.prev^.next := el2;
+        el1^.next^.prev := el1
+      end
+    end else begin
+      // Для не соседних элементов.
+      if (el1^.next = nil) then
+        exchange(el2, el1)
+      else begin
+        new(buf);
+        // Сохранение связей первого элемента.
+        buf^.next := el1^.next;
+        buf^.prev := el1^.prev;
+
+        // Перенос первого элемента на место второго.
+        el1^.prev := el2^.prev;
+        el1^.next := el2^.next;
+        el2^.prev^.next := el1;
+        if (el2^.next <> nil) then
+          el2^.next^.prev := el1;
+
+        // Перенос второго элемента на место первого.
+        el2^.next := buf^.next;
+        el2^.prev := buf^.prev;
+        buf^.next^.prev := el2;
+        if (buf^.prev <> nil) then
+          buf^.prev^.next := el2;
+
+        // Очистка памяти и обнуление ссылки буферной переменной.
+        dispose(buf);
+        buf := nil;
+      end
     end
-  end else begin
-    // Для не соседних элементов.
-    // Перенос первого элемента на место второго.
-    el1^.next := el2^.next;
-    el1^.prev := el2^.prev;
-    el2^.prev^.next := el1;
-    el2^.next^.prev := el1;
+  end
+end;
 
-    // Перенос второго элемента на место первого.
-    el2^.next := buf^.next;
-    el2^.prev := buf^.prev;
-    buf^.prev^.next := el2;
-    buf^.next^.prev := el2
-  end;
+// Функция valueExchange не изменяет порядок элементов в очереди, но
+// производит перенос значений между выбранными элементами.
+procedure valueExchange(el1, el2: pQu);
+  var buf: dateYMD;
+begin
+  // Передача параметра по значению происходит, так как нет необходимости
+  // изменять значение переданных указателей.
+  buf.day := el1^.date.day;
+  buf.month := el1^.date.month;
+  buf.year := el1^.date.year;
+
+  el1^.date.day := el2^.date.day;
+  el1^.date.month := el2^.date.month;
+  el1^.date.year := el2^.date.year;
+
+  el2^.date.day := buf.day;
+  el2^.date.month := buf.month;
+  el2^.date.year := buf.year;
+
 end;
 
 // Выяснение длины очереди.
@@ -114,12 +171,47 @@ begin
   end
 end;
 
+function qPartLen(start, finish: pQu): Integer;
+  var len: Integer;
+      b: Boolean;
+begin
+  // Функция высчитывает число элементов, из которых состоит интервал.
+  len := 1;
+
+  While (start <> finish) do begin
+    inc(len);
+
+    if (start^.next = nil) then begin
+      if (start <> finish) then len := 0;
+    end else start := start^.next;
+  end;
+
+  qPartLen := len
+end;
+
+procedure printOut(q: pQu);
+  var i: Integer;
+begin
+  if (q <> nil) then begin
+    i := 1;
+    while q^.prev <> nil do
+      q := q^.prev;
+
+    Repeat
+      writeln(i, '. ', q^.date.day, '.', q^.date.month, '.', q^.date.year);
+      q := q^.next;
+      inc(i)
+    Until q = nil;
+  end else writeln('Empty queue input.')
+end;
+
 ///////////////////////////////////////////////////////
 // ФУНКЦИИ И ПРОЦЕДУРЫ ДЛЯ ОБРАБОТКИ ВХОДЯЩЕГО ФАЙЛА //
 ///////////////////////////////////////////////////////
 
-// Функция charsValues проверяет, являются ли, начиная с номера start и продолжительностью numOfC
-// символы в строке line цифрами или не цифрами (в зависимости от параметра cType).
+// Функция charsValues проверяет, являются ли, начиная с номера start и
+// продолжительностью numOfC символы в строке line цифрами или не цифрами (в
+// зависимости от параметра cType).
 function charsValues(var line: String; cType: NumOrNot; var start: Integer; numOfC: Integer): Boolean;
   var zero, nine: Char;
       i: Integer;
@@ -148,11 +240,9 @@ function correctDate(var line: String; len: Integer): Boolean;
       b: Boolean;
 begin
   i := 1;
-  b := FALSE;
-  If (len = 8) Then
-    b := charsValues(line, num, i, 2) AND charsValues(line, notnum, i, 1) AND charsValues(line, num, i, 2) AND charsValues(line, notnum, i, 1) AND charsValues(line, num, i, 2)
-  else If (len = 10) Then
-    b := charsValues(line, num, i, 2) AND charsValues(line, notnum, i, 1) AND charsValues(line, num, i, 2) AND charsValues(line, notnum, i, 1) AND charsValues(line, num, i, 4);
+  
+  b := charsValues(line, num, i, 2) AND charsValues(line, notnum, i, 1) AND charsValues(line, num, i, 2) AND charsValues(line, notnum, i, 1) AND charsValues(line, num, i, len - 6);
+
   correctDate := b;
 end;
 
@@ -163,19 +253,18 @@ procedure checkFill(var testFile: text; var line: String; var q, p: pQu);
 begin
   len := Length(line);
 
-  If (len <= 10) Then
-    // Функция для проверки соответствия даты формату.
-    If correctDate(line, len) Then begin
-      line[3] := '.';
-      line[6] := '.';
-      Writeln(testFile, line);
+  // Функция для проверки соответствия даты формату.
+  If correctDate(line, len) Then begin
+    line[3] := '.';
+    line[6] := '.';
+    Writeln(testFile, line);
 
-      val(copy(line, 7, len - 7 + 1), date.year, err);
-      val(copy(line, 4, 2), date.month, err);
-      val(copy(line, 1, 2), date.day, err);
+    val(copy(line, 7, len - 7 + 1), date.year, err);
+    val(copy(line, 4, 2), date.month, err);
+    val(copy(line, 1, 2), date.day, err);
 
-      push(q, p, date);
-    End
+    push(q, p, date);
+  End
 End;
 
 // Функция для чтения, обработки и передачи дат.
@@ -203,57 +292,65 @@ end;
 // Численная переменная numOfComparisons существует для подсчёта числа сравнений,
 // выполненных во время работы алгоритма.
 // arg1 и arg2 представляют собой сравниваемые значения
-function dateComparison(var numOfComparisons: Integer; var arg1, arg2: dateYMD; bigger: Boolean): Boolean;
+function dateIsLater(var numOfComparisons: Integer; var arg1, arg2: dateYMD): Boolean;
 begin
   inc(numOfComparisons);
-  dateComparison := bigger AND ((arg1.year > arg2.year) OR
-                               ((arg1.year = arg2.year) AND (arg1.month > arg2.month)) OR
-                               ((arg1.year = arg2.year) AND (arg1.month = arg2.month) AND (arg1.day > arg2.day)));
+  dateIsLater := ((arg1.year > arg2.year) OR
+                 ((arg1.year = arg2.year) AND (arg1.month > arg2.month)) OR
+                 ((arg1.year = arg2.year) AND (arg1.month = arg2.month) AND (arg1.day > arg2.day)));
+end;
+
+procedure countAndValueExchange(var numOfShifts: Integer; el1, el2: pQu);
+begin
+  inc(numOfShifts);
+  valueExchange(el1, el2);
 end;
 
 // Алгоритм быстрой сортировки из книги "Искусство программирования"
 // Дональда Кнута, переделанный для сортировки динамической структуры.
 procedure quickSort(var q, p: pQu);
-  var N, l, r, i, j, K: pQu;
-      buf: dateYMD;
+  var N, l, r, i, j: pQu;
+      temp, buf, K: dateYMD;
 begin
-  N := p;
+  // Подготовка поступившей очереди к обработке алгоритмом.
+  // Добавление искусственного ключа слева.
+  temp.day := MinInt;
+  temp.month := MinInt;
+  temp.year := MinInt;
+  shift(q, temp);
+  // Добавление искусственного ключа справа.
+  temp.day := MaxInt;
+  temp.month := MaxInt;
+  temp.year := MaxInt;
+  push(q, p, temp);
+
   // Q1
-  l := q;
-  r := N;
+  l := q^.next; // l := 1;
+  r := p^.prev; // l := N;
 
   // Q2
   i := l;
-  j := l;
-  K := l;
+  j := r^.next;
+  K := l^.date;
 
-  While not((i = j) OR (i^.prev = j)) Do begin
+  // Q5
+  While (j <> i) (*OR (j^.next <> i)*) do begin
     // Q3
     Repeat
       i := i^.next;
-    Until dateComparison(numOfComparisons, K^.date, i^.date, FALSE) OR
-          (i = j) OR (i^.prev = j);
+    Until dateIsLater(numOfComparisons, i^.date, K) OR (i = j) OR (i^.prev = j);
+    Writeln(i^.date.year);
 
     // Q4
     Repeat
       j := j^.prev;
-    Until dateComparison(numOfComparisons, K^.date, i^.date, TRUE) OR
-          (j = i) OR (j^.next = i);
-
-    // Q5
-    if (i = j) OR (i^.prev = j) Then begin
-      buf := l^.date;
-      l^.date := j^.date;
-      j^.date := buf;
+    Until j = i (*dateIsLater(numOfComparisons, K, j^.date) OR (j = i) OR (j^.next = i)*);
+    Writeln(j^.date.year);
+    
     // Q6
-    end else begin
-      buf := i^.date;
-      i^.date := j^.date;
-      j^.date := buf;
-    end;
+    // if (j <> i) OR (j^.next <> i) then
+      countAndValueExchange(numOfShifts, i, j);
   end;
-
-
 end;
 
 begin
@@ -266,10 +363,12 @@ begin
 
   close(unrefinedFile);
 
-  Writeln('Год: ', pointer^.date.year, ', месяц: ', pointer^.date.month, ', день: ', pointer^.date.day, '.');
-  Writeln();
-  Writeln(qLen(dates));
-
+  printOut(dates);
+  quickSort(dates, pointer);
+  printOut(dates);
+  writeln();
+  writeln(numOfComparisons);
+  writeln(numOfShifts);
   readln()
 
 End.
